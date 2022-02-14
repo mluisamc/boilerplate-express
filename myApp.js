5a0dcd90a693383cc56eb61d544954e3ac63ccc3
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 10:19:32 +0100

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -36,9 +36,11 @@ app.get('/:word/echo', function (req, res) {
     res.json({"echo": req.params.word})
 })
 
-app.get('/name', function (req, res) {
+app.route('/name').get(function (req, res) {
     res.json({ "name": req.query.first + " " + req.query.last})
-})
+}).post((function (req, res) {
+    res.json({ "name": req.body.first + " " + req.body.last})
+}))
 
 
 
