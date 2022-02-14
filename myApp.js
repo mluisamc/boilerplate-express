be4898e7f14bd285b9d7785cf33c7d72b40f6f1e
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 09:41:47 +0100

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -25,6 +25,13 @@ app.get('/json', function (req, res) {
     
 })
 
+app.get('/now', function(req, res, next) {  
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send({time: req.time});
+});
+
 
 
 
