f7f02deec63fe978301f97169d09d26daec33495
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 09:14:07 +0100

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ require('dotenv').config()
 
 console.log("Hello World");
 
+app.use('/', function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
